8d6bdd5352be2de63f1ecc4c4f2ce3d60f8da46a
Christoph Kogler <christophkogler2@gmail.com>
2026-05-07 15:07:16 -0700

    Optimize jelly ingredient search

--- 1.6/Source/Utilities/XMTUtility.cs
+++ 1.6/Source/Utilities/XMTUtility.cs
@@ -643,11 +643,12 @@ namespace Xenomorphtype
         }
         public static Thing SearchRegionsForJellyMakable(Region region, Pawn pawn, CompJellyMaker jellyMaker, bool requireLightLevel = false)
         {
-            if(pawn == null)
+            if(region == null || pawn == null || jellyMaker == null)
             {
                 return null;
             }
             Thing found = null;
+            Dictionary<ThingDef, bool> canMakeIntoJellyByDef = new Dictionary<ThingDef, bool>();
           
             TraverseParms traverseParams = TraverseParms.For(pawn);
 
@@ -662,17 +663,34 @@ namespace Xenomorphtype
                 {
                     Thing thing = list[i];
 
-                    if (jellyMaker.CanMakeIntoJelly(thing) &&
-                        (!requireLightLevel || CompJellyMaker.IsProperLightLevelForFeralJelly(thing.PositionHeld, thing.MapHeld)) &&
-                        FeralJobUtility.IsThingAvailableForJobBy(pawn, thing))
+                    if (thing is Corpse corpse && corpse.GetRotStage() == RotStage.Dessicated)
                     {
-                        if(!FeralJobUtility.IsThingAvailableForJobBy(pawn,thing))
-                        {
-                            continue;
-                        }
-                        found = thing;
-                        return true;
+                        continue;
                     }
+
+                    if (!canMakeIntoJellyByDef.TryGetValue(thing.def, out bool canMakeIntoJelly))
+                    {
+                        canMakeIntoJelly = jellyMaker.CanMakeIntoJelly(thing.def);
+                        canMakeIntoJellyByDef[thing.def] = canMakeIntoJelly;
+                    }
+
+                    if (!canMakeIntoJelly)
+                    {
+                        continue;
+                    }
+
+                    if (requireLightLevel && !CompJellyMaker.IsProperLightLevelForFeralJelly(thing.PositionHeld, thing.MapHeld))
+                    {
+                        continue;
+                    }
+
+                    if (!FeralJobUtility.IsThingAvailableForJobBy(pawn, thing))
+                    {
+                        continue;
+                    }
+
+                    found = thing;
+                    return true;
                 }
                 return false;
             };
