51578c0bcef921ea72a5a324b23344683882c08e
Christoph Kogler <christophkogler2@gmail.com>
2026-05-16 01:07:49 -0700

    Add prisoner release command

---
+++ 1.6/Source/AI/JobDriver/WorkJobs/JobDriver_ReleasePrisonerByXenomorph.cs
@@ -0,0 +1,118 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace Xenomorphtype
+{
+    public class JobDriver_ReleasePrisonerByXenomorph : JobDriver_ClimbToPosition
+    {
+        private const TargetIndex PrisonerInd = TargetIndex.A;
+        private const TargetIndex ReleaseCellInd = TargetIndex.B;
+        private const float GrabTicksFinish = 30f;
+
+        private float grabTicks;
+        private float grabProgress;
+        private bool failedGrab;
+
+        protected override IntVec3 FinalGoalCell => job.GetTarget(ReleaseCellInd).Cell;
+        private Pawn Prisoner => job.GetTarget(PrisonerInd).Pawn;
+
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return true;
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            this.FailOnDestroyedOrNull(PrisonerInd);
+            this.FailOnAggroMentalState(PrisonerInd);
+            this.AddFailCondition(() => failedGrab || Prisoner == null || !Prisoner.IsPrisonerOfColony || !FinalGoalCell.IsValid);
+
+            yield return Toils_Goto.GotoThing(PrisonerInd, PathEndMode.ClosestTouch);
+            yield return AttemptGrab();
+            yield return Toils_Haul.StartCarryThing(PrisonerInd);
+            yield return Toils_Haul.CarryHauledThingToCell(ReleaseCellInd, PathEndMode.OnCell);
+            yield return DropPrisoner();
+            yield return ReleasePrisoner();
+        }
+
+        private Toil AttemptGrab()
+        {
+            Toil toil = ToilMaker.MakeToil("AttemptReleaseGrab");
+            toil.atomicWithPrevious = true;
+            toil.initAction = delegate
+            {
+                CompMatureMorph matureMorph = pawn.GetMorphComp();
+                if (matureMorph != null && !matureMorph.InitiateGrabCheck(Prisoner))
+                {
+                    failedGrab = true;
+                }
+            };
+            toil.tickAction = delegate
+            {
+                grabTicks += 1f;
+                grabProgress = grabTicks / GrabTicksFinish;
+                if (grabTicks >= GrabTicksFinish)
+                {
+                    ReadyForNextToil();
+                }
+            };
+            toil.AddFinishAction(delegate
+            {
+                CompMatureMorph matureMorph = pawn.GetMorphComp();
+                if (matureMorph != null && grabProgress >= 1f && !failedGrab)
+                {
+                    matureMorph.TryGrab(Prisoner);
+                }
+            });
+            toil.WithProgressBar(PrisonerInd, () => grabProgress);
+            toil.defaultCompleteMode = ToilCompleteMode.Never;
+            return toil;
+        }
+
+        private Toil DropPrisoner()
+        {
+            Toil toil = ToilMaker.MakeToil("DropReleasedPrisoner");
+            toil.initAction = delegate
+            {
+                if (pawn.carryTracker.CarriedThing != null)
+                {
+                    pawn.carryTracker.TryDropCarriedThing(FinalGoalCell, ThingPlaceMode.Near, out Thing _);
+                }
+            };
+            toil.defaultCompleteMode = ToilCompleteMode.Instant;
+            return toil;
+        }
+
+        private Toil ReleasePrisoner()
+        {
+            Toil toil = ToilMaker.MakeToil("ReleasePrisoner");
+            toil.initAction = delegate
+            {
+                Pawn prisoner = Prisoner;
+                if (prisoner == null)
+                {
+                    return;
+                }
+
+                prisoner.MapHeld.designationManager.TryRemoveDesignationOn(prisoner, XenoWorkDefOf.XMT_Release);
+                GenGuest.PrisonerRelease(prisoner);
+
+                if (!PawnBanishUtility.WouldBeLeftToDie(prisoner, prisoner.Map.Tile))
+                {
+                    GenGuest.AddHealthyPrisonerReleasedThoughts(prisoner);
+                }
+
+                QuestUtility.SendQuestTargetSignals(prisoner.questTags, "Released", prisoner.Named("SUBJECT"));
+
+                if (prisoner.Spawned && prisoner.Position.OnEdge(prisoner.Map))
+                {
+                    prisoner.ExitMap(false, Rot4.Invalid);
+                }
+            };
+            toil.defaultCompleteMode = ToilCompleteMode.Instant;
+            return toil;
+        }
+    }
+}
---
+++ 1.6/Source/UI/Designators/Designator_ReleasePrisoner.cs
@@ -0,0 +1,100 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace Xenomorphtype
+{
+    internal class Designator_ReleasePrisoner : Designator
+    {
+        public override bool DragDrawMeasurements => true;
+        public override DrawStyleCategoryDef DrawStyleCategory => DrawStyleCategoryDefOf.Areas;
+
+        protected override DesignationDef Designation => XenoWorkDefOf.XMT_Release;
+
+        public override bool Disabled
+        {
+            get
+            {
+                return !XMTUtility.PlayerXenosOnMap(Find.CurrentMap);
+            }
+        }
+
+        public override bool Visible
+        {
+            get
+            {
+                return XMTUtility.PlayerXenosOnMap(Find.CurrentMap);
+            }
+        }
+
+        public Designator_ReleasePrisoner()
+        {
+            defaultLabel = "XMT_CommandReleasePrisoner".Translate();
+            defaultDesc = "XMT_CommandReleasePrisonerDescription".Translate();
+            icon = ContentFinder<Texture2D>.Get("UI/Designators/Break");
+            soundDragSustain = SoundDefOf.Designate_DragStandard;
+            soundDragChanged = SoundDefOf.Designate_DragStandard_Changed;
+            useMouseIcon = true;
+            soundSucceeded = SoundDefOf.Designate_Hunt;
+            hotKey = KeyBindingDefOf.Misc12;
+        }
+
+        public override AcceptanceReport CanDesignateCell(IntVec3 c)
+        {
+            if (!c.InBounds(base.Map))
+            {
+                return false;
+            }
+
+            if (!ReleasablePrisonersInCell(c).Any())
+            {
+                return "XMT_MessageMustDesignateReleasablePrisoner".Translate();
+            }
+
+            return true;
+        }
+
+        public override void DesignateSingleCell(IntVec3 loc)
+        {
+            foreach (Pawn prisoner in ReleasablePrisonersInCell(loc))
+            {
+                DesignateThing(prisoner);
+            }
+        }
+
+        public override AcceptanceReport CanDesignateThing(Thing t)
+        {
+            if (t is Pawn pawn && pawn.IsPrisonerOfColony && !pawn.Dead && base.Map.designationManager.DesignationOn(pawn, Designation) == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override void DesignateThing(Thing t)
+        {
+            base.Map.designationManager.RemoveAllDesignationsOn(t);
+            base.Map.designationManager.AddDesignation(new Designation(t, Designation));
+        }
+
+        private IEnumerable<Pawn> ReleasablePrisonersInCell(IntVec3 c)
+        {
+            if (c.Fogged(base.Map))
+            {
+                yield break;
+            }
+
+            List<Thing> thingList = c.GetThingList(base.Map);
+            for (int i = 0; i < thingList.Count; i++)
+            {
+                if (CanDesignateThing(thingList[i]).Accepted)
+                {
+                    yield return (Pawn)thingList[i];
+                }
+            }
+        }
+    }
+}
